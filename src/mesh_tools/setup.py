77ac0553d987ecb7352f3391dd9e98a1636421ea
andy22b <andy22b@gmail.com>
2022-08-05 17:36:59 +1200

    Setup files for reading tsurf

---
+++ src/mesh_tools/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='tri_dislocation_tools',
+      version='0.1',
+      description='Read and write of triangular-based meshes',
+      author='Andy Howell',
+      author_email='a.howell@gns.cri.nz',
+      packages=find_packages(),
+      )
